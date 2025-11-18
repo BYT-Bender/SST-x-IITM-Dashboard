0ffc7284c3e1e598c6ee29deb600c5afee7796f2
Byte Bender <136265142+BYT-Bender@users.noreply.github.com>
2025-11-18 19:07:42 +0530

    Update auth.js

--- assets/scripts/auth.js
+++ assets/scripts/auth.js
@@ -1,8 +1,3 @@
-const SUPABASE_URL = "https://fvvfmyizwilosmbhtlhh.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2dmZteWl6d2lsb3NtYmh0bGhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyMTg2MTAsImV4cCI6MjA3Njc5NDYxMH0._dPnV9wgBZZKvFR-zSZPp_FFQJ5Rf1akuMiS8maRhIs";
-
-const client = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
 const loginBtn = document.getElementById("google-login");
 
 loginBtn.addEventListener("click", async () => {
